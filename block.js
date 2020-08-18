81a14013698ae5101e88f7bfff74dee251c8bb7a
hbendis1 <bendishoxha@hotmail.com>
2020-08-18 23:00:38 +0200

    commit three

--- block.js
+++ block.js
@@ -1,22 +1,27 @@
 class Block {
-    constructor(timestamp, lastHash, hash, data) {
-      this.timestamp = timestamp;
-      this.lastHash = lastHash;
-      this.hash = hash;
-      this.data = data;
+    constructor({timestamp, lastHash, hash, data}) {
+     this.timestamp = timestamp;
+     this.lastHash = lastHash;
+     this.hash = hash;
+     this.data = data;
     }
+  }
+    //const block1 = new Block({data:'foo-data', lastHash:'foo-hash', hash:'foo-data',timestamp:'01/01/01'});
 
-    toString() {
-        return `Block -
-              Timestamp : ${this.timestamp}
-              Last Hash : ${this.lastHash.substring(0, 10)}
-              Hash      : ${this.hash.substring(0, 10)}
-              Data      : ${this.data}`;
-      }
-      static genesis() {
-        return new this('Genesis time', '-----', 'f1r57-h45h', []);
-      }
-    }
+    //console.log('block1', block1);
+
+
+    //toString() {
+        //return `Block -
+        //      Timestamp : ${this.timestamp}
+         //     Last Hash : ${this.lastHash.substring(0, 10)}
+          //    Hash      : ${this.hash.substring(0, 10)}
+           //   Data      : ${this.data}`;
+     // }
+     // static genesis() {
+      //  return new this('Genesis time', '-----', 'f1r57-h45h', []);
+     // }
+    
 
     module.exports  = Block;
 
