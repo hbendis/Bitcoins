be8cbf7e388c0569ea61a997ab53e852eb4576dc
hbendis1 <bendishoxha@hotmail.com>
2020-08-19 14:21:11 +0200

    commit four

--- block.js
+++ block.js
@@ -1,3 +1,6 @@
+
+const {GENESIS_DATA} = require('./config');
+const cryptoHash = require('./crypto-hash');
 class Block {
     constructor({timestamp, lastHash, hash, data}) {
      this.timestamp = timestamp;
@@ -5,22 +8,22 @@ class Block {
      this.hash = hash;
      this.data = data;
     }
+        static genesis() {
+      return new this(GENESIS_DATA);
+     }
+     static mineBlock({ lastBlock, data}) {
+       const timestamp = Date.now();
+       const lastHash = lastBlock.hash;
+       return new this({
+        timestamp: Date.now(),
+        lastHash: lastBlock.hash,
+        data,
+        hash: cryptoHash(timestamp, lastHash, data)
+       });
+
+     }
   }
-    //const block1 = new Block({data:'foo-data', lastHash:'foo-hash', hash:'foo-data',timestamp:'01/01/01'});
 
-    //console.log('block1', block1);
-
-
-    //toString() {
-        //return `Block -
-        //      Timestamp : ${this.timestamp}
-         //     Last Hash : ${this.lastHash.substring(0, 10)}
-          //    Hash      : ${this.hash.substring(0, 10)}
-           //   Data      : ${this.data}`;
-     // }
-     // static genesis() {
-      //  return new this('Genesis time', '-----', 'f1r57-h45h', []);
-     // }
     
 
     module.exports  = Block;
