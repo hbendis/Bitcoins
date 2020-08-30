04f814530ea2d72d46133b20d410725ae15f21e1
hbendis1 <bendishoxha@hotmail.com>
2020-08-30 19:30:21 +0200

    commit 10

--- scripts/average-work.js
+++ scripts/average-work.js
@@ -1,8 +1,8 @@
-const Blockchain = require('./blockchain');
+const Blockchain = require('../blockchain');
 
-const blockchain = new Blockchain;
+const blockchain = new Blockchain();
 
-blockchain.addBlock({ data: 'initial'});
+blockchain.addBlock({ data: 'initial' });
 
 console.log('first block', blockchain.chain[blockchain.chain.length-1]);
 
@@ -10,10 +10,10 @@ let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
 
 const times = [];
 
-for ( let i=0; i<10000; i++) {
+for (let i = 0; i < 10000; i++) {
     prevTimestamp = blockchain.chain[blockchain.chain.length-1].timestamp;
 
-    blockchain.addBlock({ data: `block ${i}`}); 
+    blockchain.addBlock({ data: `block ${i}`});
     nextBlock = blockchain.chain[blockchain.chain.length-1];
 
     nextTimestamp = nextBlock.timestamp;
@@ -22,5 +22,6 @@ for ( let i=0; i<10000; i++) {
 
     average = times.reduce((total, num) => (total + num))/times.length;
 
-   console.log(`Time to mine block: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms`);
+    console.log(`Time to mine block: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms`);
+    
 }
