ef7db644fcbe24d716f7a038caaae8c52901a0ba
hbendis1 <bendishoxha@hotmail.com>
2020-08-30 20:57:15 +0200

    commit 12

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,7 +1,7 @@
 const Block = require('./block');
 const Transaction = require('../wallet/transaction');
 const Wallet = require('../wallet');
-const cryptoHash = require('../util/crypto-hash');
+const { cryptoHash } = require('../util');
 const { REWARD_INPUT, MINING_REWARD } = require('../config');
 
 class Blockchain {
--- blockchain/index.test.js
+++ blockchain/index.test.js
@@ -1,6 +1,6 @@
 const Blockchain = require('./index');
 const Block = require('./block');
-const cryptoHash = require('../util/crypto-hash');
+const { cryptoHash } = require('../util');
 const Wallet = require('../wallet');
 const Transaction = require('../wallet/transaction');
 
